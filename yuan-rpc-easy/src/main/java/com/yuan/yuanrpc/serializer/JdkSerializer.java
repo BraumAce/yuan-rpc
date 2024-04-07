3e51b5cadbe447a0d74b9068c181a0ad58318cf3
BraumAce <1693717911@qq.com>
2024-04-07 19:39:52 +0800

    JDK 序列化器

---
+++ yuan-rpc-easy/src/main/java/com/yuan/yuanrpc/serializer/JdkSerializer.java
@@ -0,0 +1,45 @@
+package com.yuan.yuanrpc.serializer;
+
+import java.io.*;
+
+/**
+ * JDK 序列化器
+ */
+public class JdkSerializer implements Serializer{
+    /**
+     * 序列化：将 Java 对象转换为可输出的字节数组
+     * @param object
+     * @return
+     * @param <T>
+     * @throws IOException
+     */
+    @Override
+    public <T> byte[] serializer(T object) throws IOException{
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(object);
+        objectOutputStream.close();
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * 反序列化：将字节数组转换为 Java 对象
+     * @param bytes
+     * @param type
+     * @return
+     * @param <T>
+     * @throws IOException
+     */
+    @Override
+    public <T> T deserializer(byte[] bytes, Class<T> type) throws IOException{
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        try {
+            return (T)objectInputStream.readObject();
+        } catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        } finally {
+            objectInputStream.close();
+        }
+    }
+}
