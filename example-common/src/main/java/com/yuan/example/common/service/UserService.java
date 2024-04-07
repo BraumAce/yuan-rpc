9b3966b3cf24c0807f7028272939cb657601d1e1
BraumAce <1693717911@qq.com>
2024-04-07 19:38:50 +0800

    用户服务接口

---
+++ example-common/src/main/java/com/yuan/example/common/service/UserService.java
@@ -0,0 +1,15 @@
+package com.yuan.example.common.service;
+
+import com.yuan.example.common.model.User;
+
+/**
+ * 用户服务
+ */
+public interface UserService {
+    /**
+     * 获取用户
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+}
