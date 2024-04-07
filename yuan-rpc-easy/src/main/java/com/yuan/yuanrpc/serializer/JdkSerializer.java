19e0ea5e02b304be6c107774872e96b07b1800b6
BraumAce <1693717911@qq.com>
2024-04-07 23:30:40 +0800

    JDK 序列化器

--- yuan-rpc-easy/src/main/java/com/yuan/yuanrpc/serializer/JdkSerializer.java
+++ yuan-rpc-easy/src/main/java/com/yuan/yuanrpc/serializer/JdkSerializer.java
@@ -14,7 +14,7 @@ public class JdkSerializer implements Serializer{
      * @throws IOException
      */
     @Override
-    public <T> byte[] serializer(T object) throws IOException{
+    public <T> byte[] serialize(T object) throws IOException{
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
         objectOutputStream.writeObject(object);
@@ -31,11 +31,11 @@ public class JdkSerializer implements Serializer{
      * @throws IOException
      */
     @Override
-    public <T> T deserializer(byte[] bytes, Class<T> type) throws IOException{
+    public <T> T deserialize(byte[] bytes, Class<T> type) throws IOException{
         ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
         ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
         try {
-            return (T)objectInputStream.readObject();
+            return (T) objectInputStream.readObject();
         } catch (ClassNotFoundException e){
             throw new RuntimeException(e);
         } finally {
