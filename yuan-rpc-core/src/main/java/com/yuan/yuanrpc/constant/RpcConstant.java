7ccfe808101aaf00535b0602562c3c67c3af214f
BraumAce <1693717911@qq.com>
2024-04-10 16:59:12 +0800

    RPC 相关常量

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/constant/RpcConstant.java
@@ -0,0 +1,10 @@
+package com.yuan.yuanrpc.constant;
+
+/**
+ * RPC 相关常量
+ * @author BraumAce
+ */
+public interface RpcConstant {
+    // 配置文件默认加载前缀
+    String DEFAULT_CONFIG_PREFIX = "yuan-rpc";
+}
