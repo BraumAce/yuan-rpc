eac0989f5837e9005533120f0ac26206826e88ee
BraumAce <1693717911@qq.com>
2024-04-10 21:01:01 +0800

    服务消费者示例

--- example-consumer/src/main/java/com/yuan/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/yuan/example/consumer/ConsumerExample.java
@@ -1,14 +1,35 @@
 package com.yuan.example.consumer;
 
+import com.yuan.example.common.model.User;
+import com.yuan.example.common.service.UserService;
 import com.yuan.yuanrpc.config.RpcConfig;
+import com.yuan.yuanrpc.proxy.ServiceProxyFactory;
 import com.yuan.yuanrpc.utils.ConfigUtils;
 
 /**
  * 服务消费者示例
  */
 public class ConsumerExample {
+//    public static void main(String[] args) {
+//        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+//        System.out.println(rpc);
+//    }
+
+    // 调用公共类测试 Mock 模拟服务代理
     public static void main(String[] args) {
-        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpc);
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("yuan");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null){
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
