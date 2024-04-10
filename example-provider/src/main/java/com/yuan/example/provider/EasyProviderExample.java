2a6163adbac1a71bc413e959ba23fd1b47690bcb
BraumAce <1693717911@qq.com>
2024-04-10 16:58:25 +0800

    简易服务提供者示例

--- example-provider/src/main/java/com/yuan/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/yuan/example/provider/EasyProviderExample.java
@@ -4,7 +4,6 @@ import com.yuan.example.common.service.UserService;
 import com.yuan.yuanrpc.registry.LocalRegistry;
 import com.yuan.yuanrpc.server.HttpServer;
 import com.yuan.yuanrpc.server.VertxHttpServer;
-import io.vertx.core.Vertx;
 
 /**
  * 简易服务提供者示例
