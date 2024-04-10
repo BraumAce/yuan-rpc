7ec47e44b7099fd7c993147a1fcedc92d9fb3fc6
BraumAce <1693717911@qq.com>
2024-04-10 21:01:41 +0800

    update 服务代理工厂

--- yuan-rpc-core/src/main/java/com/yuan/yuanrpc/proxy/ServiceProxyFactory.java
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/proxy/ServiceProxyFactory.java
@@ -16,6 +16,21 @@ public class ServiceProxyFactory {
         return (T) Proxy.newProxyInstance(
                 serviceClass.getClassLoader(),
                 new Class[]{serviceClass},
-                new ServiceProxy());
+                new ServiceProxy()
+        );
+    }
+
+    /**
+     * 根据服务类获取 Mock 代理对象
+     * @param serviceClass
+     * @return
+     * @param <T>
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass){
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy()
+        );
     }
 }
