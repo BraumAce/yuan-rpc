7a9d192711e18606730a18e0f7750d214c8598df
BraumAce <1693717911@qq.com>
2024-04-10 16:59:01 +0800

    RPC 框架配置

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/config/RpcConfig.java
@@ -0,0 +1,21 @@
+package com.yuan.yuanrpc.config;
+
+import lombok.Data;
+
+/**
+ * RPC 框架配置
+ */
+@Data
+public class RpcConfig {
+    // 名称
+    private String name = "yuan-rpc";
+
+    // 版本号
+    private String version = "1.0";
+
+    // 服务器主机名
+    private String serverhost = "localhost";
+
+    // 服务器端口号
+    private Integer serverPort = 8080;
+}
