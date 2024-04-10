24eca2f1c37ebfb47827d520c62dc862f72e2f44
BraumAce <1693717911@qq.com>
2024-04-10 16:58:37 +0800

    服务提供者示例

---
+++ example-provider/src/main/java/com/yuan/example/provider/ProviderExample.java
@@ -0,0 +1,24 @@
+package com.yuan.example.provider;
+
+import com.yuan.example.common.service.UserService;
+import com.yuan.yuanrpc.RpcApplication;
+import com.yuan.yuanrpc.registry.LocalRegistry;
+import com.yuan.yuanrpc.server.HttpServer;
+import com.yuan.yuanrpc.server.VertxHttpServer;
+
+/**
+ * 服务提供者示例
+ */
+public class ProviderExample {
+    public static void main(String[] args){
+        // RPC 框架初始化
+        RpcApplication.init();
+
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
+        // 启动服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+    }
+}
