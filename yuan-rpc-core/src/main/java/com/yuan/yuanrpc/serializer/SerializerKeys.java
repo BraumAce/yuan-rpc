f9d51d4ccbc5e05a74013d0e8d36027b036e508c
BraumAce <1693717911@qq.com>
2024-04-11 20:57:31 +0800

    序列化器键名

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/serializer/SerializerKeys.java
@@ -0,0 +1,15 @@
+package com.yuan.yuanrpc.serializer;
+
+/**
+ * 序列化器键名
+ */
+public interface SerializerKeys {
+
+    String JDK = "jdk";
+
+    String JSON = "json";
+
+    String Kryo = "kryo";
+
+    String Hessian = "hessian";
+}
