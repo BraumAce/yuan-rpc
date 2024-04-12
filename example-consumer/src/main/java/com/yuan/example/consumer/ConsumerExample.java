ed3a664dfe5d54bbbf93795ce6eeb5d10ef4f7bc
BraumAce <1693717911@qq.com>
2024-04-12 23:57:45 +0800

    服务消费者示例

--- example-consumer/src/main/java/com/yuan/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/yuan/example/consumer/ConsumerExample.java
@@ -2,9 +2,7 @@ package com.yuan.example.consumer;
 
 import com.yuan.example.common.model.User;
 import com.yuan.example.common.service.UserService;
-import com.yuan.yuanrpc.config.RpcConfig;
-import com.yuan.yuanrpc.proxy.ServiceProxyFactory;
-import com.yuan.yuanrpc.utils.ConfigUtils;
+import com.yuan.yuanrpc.utils.proxy.ServiceProxyFactory;
 
 /**
  * 服务消费者示例
