a76c729007e47a2cd289b6c396c6920d4798e324
BraumAce <1693717911@qq.com>
2024-04-17 23:27:53 +0800

    upload 注册中心测试

---
+++ yuan-rpc-core/src/test/java/com/yuan/yuanrpc/registry/RegistryTest.java
@@ -0,0 +1,67 @@
+package com.yuan.yuanrpc.registry;
+
+import com.yuan.yuanrpc.config.RegistryConfig;
+import com.yuan.yuanrpc.model.ServiceMetaInfo;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * 注册中心测试
+ */
+public class RegistryTest {
+    final Registry registry = new EtcdRegistry();
+
+    @Before
+    public void init(){
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress("http://localhost:2379");
+        registry.init(registryConfig);
+    }
+
+    @Test
+    public void registry() throws Exception{
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.1");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1234);
+        registry.register(serviceMetaInfo);
+
+        serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.2");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1235);
+        registry.register(serviceMetaInfo);
+
+        serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("2.0");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1234);
+        registry.register(serviceMetaInfo);
+    }
+
+    @Test
+    public void unRegister() throws Exception {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.1");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1234);
+        registry.register(serviceMetaInfo);
+    }
+
+    @Test
+    public void serviceDiscovery(){
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.1");
+        String serviceKey = serviceMetaInfo.getServiceKey();
+        List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceKey);
+        Assert.assertNotNull(serviceMetaInfoList);
+    }
+}
