159c798b0e570d8b13257ba33034be263c2e82be
BraumAce <1693717911@qq.com>
2024-04-17 23:40:35 +0800

    update RpcRequest

--- yuan-rpc-core/src/main/java/com/yuan/yuanrpc/model/RpcRequest.java
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/model/RpcRequest.java
@@ -1,5 +1,6 @@
 package com.yuan.yuanrpc.model;
 
+import com.yuan.yuanrpc.constant.RpcConstant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,4 +28,8 @@ public class RpcRequest implements Serializable {
 
     // 参数列表
     private Object[] args;
+
+    // 服务版本
+    private String serviceVersion = RpcConstant.DEFAULT_SERVICE_VERSION;
+
 }
