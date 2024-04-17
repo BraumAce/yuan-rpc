488e517cecfa9541e1598712c74e1721c283fcbc
BraumAce <1693717911@qq.com>
2024-04-17 23:28:11 +0800

    upload 注册中心接口

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/registry/Registry.java
@@ -0,0 +1,42 @@
+package com.yuan.yuanrpc.registry;
+
+import com.yuan.yuanrpc.config.RegistryConfig;
+import com.yuan.yuanrpc.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * 注册中心
+ */
+public interface Registry {
+    /**
+     * 初始化
+     * @param registryConfig
+     */
+    void init(RegistryConfig registryConfig);
+
+    /**
+     * 注册服务 (服务端)
+     * @param serviceMetaInfo
+     * @throws Exception
+     */
+    void register(ServiceMetaInfo serviceMetaInfo) throws Exception;
+
+    /**
+     * 注销服务 (服务端）
+     * @param serviceMetaInfo
+     */
+    void unRegister(ServiceMetaInfo serviceMetaInfo);
+
+    /**
+     * 服务发现 (获取某服务的所有节点，消费端)
+     * @param serviceKey
+     * @return
+     */
+    List<ServiceMetaInfo> serviceDiscovery(String serviceKey);
+
+    /**
+     * 服务销毁
+     */
+    void destroy();
+}
