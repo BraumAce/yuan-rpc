80a69aca6bb2302e5ba14d044bc5fbc3af6ae5d4
BraumAce <1693717911@qq.com>
2024-04-17 23:28:24 +0800

    upload RPC 框架注册中心配置

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/config/RegistryConfig.java
@@ -0,0 +1,24 @@
+package com.yuan.yuanrpc.config;
+
+import lombok.Data;
+
+/**
+ * RPC 框架注册中心配置
+ */
+@Data
+public class RegistryConfig {
+    // 注册中心类别
+    private String registry = "etcd";
+    // 注册中心地址
+    private String address = "http://localhost:2380";
+
+    // 用户名
+    private String name;
+
+    // 密码
+    private String password;
+
+    // 超时时间 (毫秒)
+    private Long timeout = 10000L;
+
+}
