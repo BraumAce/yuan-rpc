f78742216c9f17638dacad8b6b1893a51fb5e529
BraumAce <1693717911@qq.com>
2024-04-17 23:42:26 +0800

    update EasyConsumerExample

--- example-consumer/src/main/java/com/yuan/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/yuan/example/consumer/EasyConsumerExample.java
@@ -2,7 +2,7 @@ package com.yuan.example.consumer;
 
 import com.yuan.example.common.model.User;
 import com.yuan.example.common.service.UserService;
-import com.yuan.yuanrpc.utils.proxy.ServiceProxyFactory;
+import com.yuan.yuanrpc.proxy.ServiceProxyFactory;
 
 /**
  * 简易服务消费者示例
