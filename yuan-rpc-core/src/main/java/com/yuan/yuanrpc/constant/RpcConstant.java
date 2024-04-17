ab8d237cc4777821b0a4775525d63bc372edf41b
BraumAce <1693717911@qq.com>
2024-04-17 23:40:49 +0800

    update RpcConstant

--- yuan-rpc-core/src/main/java/com/yuan/yuanrpc/constant/RpcConstant.java
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/constant/RpcConstant.java
@@ -7,4 +7,9 @@ package com.yuan.yuanrpc.constant;
 public interface RpcConstant {
     // 配置文件默认加载前缀
     String DEFAULT_CONFIG_PREFIX = "yuan-rpc";
+
+    /**
+     * 默认服务版本
+     */
+    String DEFAULT_SERVICE_VERSION = "1.0";
 }
