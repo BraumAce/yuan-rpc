88a969c6fdcf3b5bd510da9d64473cbf53dec5fa
BraumAce <1693717911@qq.com>
2024-04-22 18:10:39 +0800

    upload 服务注册信息类

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/model/ServiceRegisterInfo.java
@@ -0,0 +1,20 @@
+package com.yuan.yuanrpc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 服务注册信息类
+ * @param <T>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceRegisterInfo<T> {
+    // 服务名称
+    private String serviceName;
+
+    // 实现类
+    private Class<? extends T> implClass;
+}
