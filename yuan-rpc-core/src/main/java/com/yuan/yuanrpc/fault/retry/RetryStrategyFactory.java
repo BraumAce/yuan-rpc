136c1cbbce9dc7af846dd54e0cb7df41f6cab499
BraumAce <1693717911@qq.com>
2024-04-24 21:00:50 +0800

    upload 重试策略工厂

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/fault/retry/RetryStrategyFactory.java
@@ -0,0 +1,25 @@
+package com.yuan.yuanrpc.fault.retry;
+
+import com.yuan.yuanrpc.spi.SpiLoader;
+
+/**
+ * 重试策略工厂
+ */
+public class RetryStrategyFactory {
+
+    static {
+        SpiLoader.load(RetryStrategy.class);
+    }
+
+    // 默认重试器
+    private static final RetryStrategy DEFAULT_RETRY_STRATEGY = new NoRetryStrategy();
+
+    /**
+     * 获取实例
+     * @param key
+     * @return
+     */
+    public static RetryStrategy getInstance(String key) {
+        return SpiLoader.getInstance(RetryStrategy.class, key);
+    }
+}
