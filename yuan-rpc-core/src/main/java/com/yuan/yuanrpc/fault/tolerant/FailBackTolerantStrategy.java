1e99d1a25050bedc75b653b8332ce31d4e875584
BraumAce <1693717911@qq.com>
2024-04-24 20:53:05 +0800

    upload 故障恢复 - 重试策略

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/fault/tolerant/FailBackTolerantStrategy.java
@@ -0,0 +1,18 @@
+package com.yuan.yuanrpc.fault.tolerant;
+
+import com.yuan.yuanrpc.model.RpcResponse;
+
+import java.util.Map;
+
+/**
+ * 降级到其他服务 - 容错策略
+ */
+public class FailBackTolerantStrategy implements TolerantStrategy {
+
+    @Override
+    public RpcResponse doTolerant(Map<String, Object> context, Exception e) {
+        // todo 待扩展，获取降级的服务并调用
+
+        return null;
+    }
+}
