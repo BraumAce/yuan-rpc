702818f78b9b86909352a52f5363b951ef53e6df
BraumAce <1693717911@qq.com>
2024-04-24 18:12:08 +0800

    upload 负载均衡器键名常量

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/loadbalancer/LoadBalancerKeys.java
@@ -0,0 +1,16 @@
+package com.yuan.yuanrpc.loadbalancer;
+
+/**
+ * 负载均衡器键名常量
+ */
+public interface LoadBalancerKeys {
+
+    // 轮询
+    String ROUND_ROBIN = "roundRobin";
+
+    // 随机
+    String RANDOM = "random";
+
+    // 一致性 Hash
+    String CONSISTENT_HASH = "consistentHash";
+}
