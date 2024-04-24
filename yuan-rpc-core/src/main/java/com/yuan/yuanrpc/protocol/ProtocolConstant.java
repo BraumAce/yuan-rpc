3df0c5032962d2b32faae9981b0fb68e1b242a5f
BraumAce <1693717911@qq.com>
2024-04-24 11:42:09 +0800

    upload 协议常量

---
+++ yuan-rpc-core/src/main/java/com/yuan/yuanrpc/protocol/ProtocolConstant.java
@@ -0,0 +1,17 @@
+package com.yuan.yuanrpc.protocol;
+
+/**
+ * 协议常量
+ */
+public class ProtocolConstant {
+
+    // 消息头长度
+    int MESSAGE_HEADER_LENGTH = 17;
+
+    // 协议魔数
+    public static byte PROTOCOL_MAGIC = 0x1;
+
+    // 协议版本号
+    public static byte PROTOCOL_VERSION = 0x1;
+
+}
