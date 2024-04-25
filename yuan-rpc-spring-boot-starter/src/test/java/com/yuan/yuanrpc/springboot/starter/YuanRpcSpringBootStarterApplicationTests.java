572497c03978588ac4d9ac229bc80c132cef63cb
BraumAce <1693717911@qq.com>
2024-04-25 12:34:28 +0800

    upload springboot测试类

---
+++ yuan-rpc-spring-boot-starter/src/test/java/com/yuan/yuanrpc/springboot/starter/YuanRpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.yuanrpc.springboot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuanRpcSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
