b8f15b09f5f806ed3127f57f71194d7c5f729ddc
BraumAce <1693717911@qq.com>
2024-04-25 12:32:30 +0800

    简化服务提供者示例

--- example-provider/src/main/java/com/yuan/example/provider/ProviderExample.java
+++ example-provider/src/main/java/com/yuan/example/provider/ProviderExample.java
@@ -2,43 +2,34 @@ package com.yuan.example.provider;
 
 import com.yuan.example.common.service.UserService;
 import com.yuan.yuanrpc.RpcApplication;
+import com.yuan.yuanrpc.bootstrap.ProviderBootstrap;
 import com.yuan.yuanrpc.config.RegistryConfig;
 import com.yuan.yuanrpc.config.RpcConfig;
 import com.yuan.yuanrpc.model.ServiceMetaInfo;
+import com.yuan.yuanrpc.model.ServiceRegisterInfo;
+import com.yuan.yuanrpc.registry.EtcdRegistry;
 import com.yuan.yuanrpc.registry.LocalRegistry;
 import com.yuan.yuanrpc.registry.Registry;
 import com.yuan.yuanrpc.registry.RegistryFactory;
 import com.yuan.yuanrpc.server.HttpServer;
 import com.yuan.yuanrpc.server.VertxHttpServer;
+import com.yuan.yuanrpc.server.tcp.VertxTcpServer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 服务提供者示例
  */
 public class ProviderExample {
     public static void main(String[] args){
-        // RPC 框架初始化
-        RpcApplication.init();
+        // 要注册的服务
+        List<ServiceRegisterInfo<?>> serviceRegisterInfoList = new ArrayList<>();
+        ServiceRegisterInfo<UserService> serviceRegisterInfo = new ServiceRegisterInfo<>(UserService.class.getName(), UserServiceImpl.class);
+        serviceRegisterInfoList.add(serviceRegisterInfo);
 
-        // 注册服务
-        // LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
-        String serviceName = UserService.class.getName();
-        LocalRegistry.register(serviceName, UserService.class);
+        // 服务提供者初始化
+        ProviderBootstrap.init(serviceRegisterInfoList);
 
-        // 注册服务到注册中心
-        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
-        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
-        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-        serviceMetaInfo.setServiceName(serviceName);
-        serviceMetaInfo.setServiceAddress(rpcConfig.getServerHost() + ":" + rpcConfig.getServerHost());
-        try {
-            registry.register(serviceMetaInfo);
-        } catch (Exception e){
-            throw new RuntimeException(e);
-        }
-
-        // 启动 web 服务
-        HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
