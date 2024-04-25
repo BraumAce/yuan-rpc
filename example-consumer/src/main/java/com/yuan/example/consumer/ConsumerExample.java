811bbdaab4329a1db4ce674774fa18a5ea4cff77
BraumAce <1693717911@qq.com>
2024-04-25 12:33:24 +0800

    简化服务消费者示例

--- example-consumer/src/main/java/com/yuan/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/yuan/example/consumer/ConsumerExample.java
@@ -2,6 +2,7 @@ package com.yuan.example.consumer;
 
 import com.yuan.example.common.model.User;
 import com.yuan.example.common.service.UserService;
+import com.yuan.yuanrpc.bootstrap.ConsumerBootstrap;
 import com.yuan.yuanrpc.proxy.ServiceProxyFactory;
 
 /**
@@ -15,6 +16,8 @@ public class ConsumerExample {
 
     // 调用公共类测试 Mock 模拟服务代理
     public static void main(String[] args) {
+        // 服务提供者初始化
+        ConsumerBootstrap.init();
 
         // 获取代理
         UserService userService = ServiceProxyFactory.getProxy(UserService.class);
@@ -27,8 +30,5 @@ public class ConsumerExample {
         } else {
             System.out.println("user == null");
         }
-
-        long number = userService.getNumber();
-        System.out.println(number);
     }
 }
